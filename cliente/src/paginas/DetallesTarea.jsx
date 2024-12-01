e3e92c8a48abaa7403ceb51bfd79c33204405c08
ariannaoc <ariannaolivares@gmail.com>
2024-12-01 01:46:07 -0400

    comentando componenetes de la interfaz

--- cliente/src/paginas/DetallesTarea.jsx
+++ cliente/src/paginas/DetallesTarea.jsx
@@ -40,7 +40,7 @@ const bgColor = {
 
 const TABS = [
   { title: "Detalles", icon: <FaTasks /> },
-  { title: "Actividades", icon: <RxActivityLog /> },
+  //{ title: "Actividades", icon: <RxActivityLog /> },
 ];
 
 const TASKTYPEICON = {
@@ -264,91 +264,91 @@ const DetallesTarea = () => {
   );
 };
 
-const Activities = ({ activity, id }) => {
-  const [selected, setSelected] = useState(act_types[0]);
-  const [text, setText] = useState("");
-  const isLoading = false;
+//const Activities = ({ activity, id }) => {
+//  const [selected, setSelected] = useState(act_types[0]);
+//  const [text, setText] = useState("");
+//  const isLoading = false;
 
-  const handleSubmit = async () => {};
+//  const handleSubmit = async () => {};
 
-  const Card = ({ item }) => {
-    return (
-      <div className='flex space-x-4'>
-        <div className='flex flex-col items-center flex-shrink-0'>
-          <div className='w-10 h-10 flex items-center justify-center'>
-            {TASKTYPEICON[item?.type]}
-          </div>
-          <div className='w-full flex items-center'>
-            <div className='w-0.5 bg-gray-300 h-full'></div>
-          </div>
-        </div>
+//  const Card = ({ item }) => {
+//    return (
+//      <div className='flex space-x-4'>
+//        <div className='flex flex-col items-center flex-shrink-0'>
+//          <div className='w-10 h-10 flex items-center justify-center'>
+//            {TASKTYPEICON[item?.type]}
+//          </div>
+//          <div className='w-full flex items-center'>
+//            <div className='w-0.5 bg-gray-300 h-full'></div>
+//          </div>
+//        </div>
 
-        <div className='flex flex-col gap-y-1 mb-8'>
-          <p className='font-semibold'>{item?.by?.name}</p>
-          <div className='text-gray-500 space-y-2'>
-            <span className='capitalize'>{item?.type}</span>
-            <span className='text-sm'>{moment(item?.date).fromNow()}</span>
-          </div>
-          <div className='text-gray-700'>{item?.activity}</div>
-        </div>
-      </div>
-    );
-  };
+//        <div className='flex flex-col gap-y-1 mb-8'>
+//          <p className='font-semibold'>{item?.by?.name}</p>
+//          <div className='text-gray-500 space-y-2'>
+//            <span className='capitalize'>{item?.type}</span>
+//            <span className='text-sm'>{moment(item?.date).fromNow()}</span>
+//          </div>
+//          <div className='text-gray-700'>{item?.activity}</div>
+//        </div>
+//      </div>
+//    );
+//  };
 
-  return (
-    <div className='w-full flex gap-10 2xl:gap-20 min-h-screen px-10 py-8 bg-white shadow rounded-md justify-between overflow-y-auto'>
-      <div className='w-full md:w-1/2'>
-        <h4 className='text-gray-600 font-semibold text-lg mb-5'>Actividades</h4>
+//  return (
+//    <div className='w-full flex gap-10 2xl:gap-20 min-h-screen px-10 py-8 bg-white shadow rounded-md justify-between overflow-y-auto'>
+//      <div className='w-full md:w-1/2'>
+//        <h4 className='text-gray-600 font-semibold text-lg mb-5'>Actividades</h4>
 
-        <div className='w-full'>
-          {activity?.map((el, index) => (
-            <Card
-              key={index}
-              item={el}
-              isConnected={index < activity.length - 1}
-            />
-          ))}
-        </div>
-      </div>
+//        <div className='w-full'>
+//          {activity?.map((el, index) => (
+//            <Card
+//              key={index}
+//              item={el}
+//              isConnected={index < activity.length - 1}
+//            />
+//          ))}
+//        </div>
+//      </div>
 
-      <div className='w-full md:w-1/3'>
-        <h4 className='text-gray-600 font-semibold text-lg mb-5'>
-          Agregar Actividad
-        </h4>
-        <div className='w-full flex flex-wrap gap-5'>
-          {act_types.map((item, index) => (
-            <div key={item} className='flex gap-2 items-center'>
-              <input
-                type='checkbox'
-                className='w-4 h-4'
-                checked={selected === item ? true : false}
-                onChange={(e) => setSelected(item)}
-              />
-              <p>{item}</p>
-            </div>
-          ))}
-          <textarea
-            rows={10}
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder='Escribe algo...'
-            className='bg-white w-full mt-10 border border-gray-300 outline-none p-4 rounded-md focus:ring-2 ring-blue-500'
-          ></textarea>
-          {isLoading ? (
-            <Loading />
-          ) : (
-            <Button
-              type='button'
-              label='Agregar +'
-              onClick={handleSubmit}
-              className='bg-green-600 text-white rounded-lg'
-            />
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
+//      <div className='w-full md:w-1/3'>
+//        <h4 className='text-gray-600 font-semibold text-lg mb-5'>
+//          Agregar Actividad
+//        </h4>
+//        <div className='w-full flex flex-wrap gap-5'>
+//          {act_types.map((item, index) => (
+//            <div key={item} className='flex gap-2 items-center'>
+//              <input
+//                type='checkbox'
+//                className='w-4 h-4'
+//                checked={selected === item ? true : false}
+//                onChange={(e) => setSelected(item)}
+//              />
+//              <p>{item}</p>
+//            </div>
+//          ))}
+//          <textarea
+//            rows={10}
+//            value={text}
+//            onChange={(e) => setText(e.target.value)}
+//            placeholder='Escribe algo...'
+//            className='bg-white w-full mt-10 border border-gray-300 outline-none p-4 rounded-md focus:ring-2 ring-blue-500'
+//          ></textarea>
+//          {isLoading ? (
+//            <Loading />
+//          ) : (
+//            <Button
+//              type='button'
+//              label='Agregar +'
+//              onClick={handleSubmit}
+//              className='bg-green-600 text-white rounded-lg'
+//            />
+//          )}
+//        </div>
+//      </div>
+//    </div>
+//  );
+//};
 export default DetallesTarea;
 
 
